090863893ae948572a5db80f96257be77e6c9f6b
Lê Minh Hải <haileminhit@gmail.com>
2017-08-22 17:09:36 +0700

    Update class PhysicalMap : create method 3 haveCollisionWith Top, LeftWall, RightWall

--- src/com/hailm/megaman/model/PhysicalMap.java
+++ src/com/hailm/megaman/model/PhysicalMap.java
@@ -64,6 +64,103 @@ public class PhysicalMap extends GameObject {
         return null;
     }
 
+    public Rectangle haveCollisionWithTop(Rectangle rect) {
+
+        int posX1 = rect.x / tileSize;
+        posX1 -= 2;
+
+        int posX2 = (rect.x + rect.width) / tileSize;
+        posX2 += 2;
+
+        // int posY = (rect.y + rect.height)/tileSize;
+        int posY = rect.y / tileSize;
+
+        if (posX1 < 0)
+            posX1 = 0;
+
+        if (posX2 >= phys_map[0].length)
+            posX2 = phys_map[0].length - 1;
+
+        for (int y = posY; y >= 0; y--) {
+            for (int x = posX1; x <= posX2; x++) {
+
+                if (phys_map[y][x] == 1) {
+                    Rectangle r = new Rectangle((int) getPosX() + x * tileSize,
+                            (int) getPosY() + y * tileSize, tileSize, tileSize);
+                    if (rect.intersects(r))
+                        return r;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Rectangle haveCollisionWithRightWall(Rectangle rect) {
+
+        int posY1 = rect.y / tileSize;
+        posY1 -= 2;
+
+        int posY2 = (rect.y + rect.height) / tileSize;
+        posY2 += 2;
+
+        int posX1 = (rect.x + rect.width) / tileSize;
+        int posX2 = posX1 + 3;
+
+        if (posX2 >= phys_map[0].length)
+            posX2 = phys_map[0].length - 1;
+
+        if (posY1 < 0)
+            posY1 = 0;
+        if (posY2 >= phys_map.length)
+            posY2 = phys_map.length - 1;
+
+        for (int x = posX1; x <= posX2; x++) {
+            for (int y = posY1; y <= posY2; y++) {
+                if (phys_map[y][x] == 1) {
+                    Rectangle r = new Rectangle((int) getPosX() + x * tileSize,
+                            (int) getPosY() + y * tileSize, tileSize, tileSize);
+                    if (r.y < rect.y + rect.height - 1 && rect.intersects(r))
+                        return r;
+                }
+            }
+        }
+        return null;
+
+    }
+
+    public Rectangle haveCollisionWithLeftWall(Rectangle rect) {
+
+        int posY1 = rect.y / tileSize;
+        posY1 -= 2;
+
+        int posY2 = (rect.y + rect.height) / tileSize;
+        posY2 += 2;
+
+        int posX1 = (rect.x + rect.width) / tileSize;
+        int posX2 = posX1 - 3;
+        if (posX2 < 0)
+            posX2 = 0;
+
+        if (posY1 < 0)
+            posY1 = 0;
+
+        if (posY2 >= phys_map.length)
+            posY2 = phys_map.length - 1;
+
+        for (int x = posX1; x >= posX2; x--) {
+            for (int y = posY1; y <= posY2; y++) {
+                if (phys_map[y][x] == 1) {
+                    Rectangle r = new Rectangle((int) getPosX() + x * tileSize,
+                            (int) getPosY() + y * tileSize, tileSize, tileSize);
+                    if (r.y < rect.y + rect.height - 1 && rect.intersects(r))
+                        return r;
+                }
+            }
+        }
+        return null;
+
+    }
+
     @Override
     public void update() {
         // TODO Auto-generated method stub
